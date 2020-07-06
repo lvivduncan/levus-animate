1f32dfab282c7546038de6b075a49ad699662388
lvivduncan <lvivduncan@gmail.com>
2020-07-06 19:11:23 +0300

    finish

--- js/levus-animate.js
+++ js/levus-animate.js
@@ -10,4 +10,8 @@ const height = document.documentElement.clientHeight;
 // усі елементи, які мають бути анімовані
 const elements = document.querySelectorAll('.levus-animate');
 
+// робимо батьківський елемент оверфлоу:хідден, щоб сторінку не рвало
+// todo: зробити присвоєння 1 раз
+elements.forEach(element => element.parentElement.classList.add('hidden'));
+
 window.addEventListener('scroll', () => elements.forEach(item => item.getBoundingClientRect().top < height && item.classList.add('show')));
