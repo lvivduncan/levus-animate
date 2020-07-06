9ad575b118fccc22201f26a1e7b85a4217262a16
lvivduncan <lvivduncan@gmail.com>
2020-07-06 19:20:44 +0300

    del todo; not relevant

--- js/levus-animate.js
+++ js/levus-animate.js
@@ -11,7 +11,6 @@ const height = document.documentElement.clientHeight;
 const elements = document.querySelectorAll('.levus-animate');
 
 // робимо батьківський елемент оверфлоу:хідден, щоб сторінку не рвало
-// todo: зробити присвоєння 1 раз
 elements.forEach(element => element.parentElement.classList.add('hidden'));
 
 window.addEventListener('scroll', () => elements.forEach(item => item.getBoundingClientRect().top < height && item.classList.add('show')));
